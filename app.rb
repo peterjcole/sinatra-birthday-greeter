e459d06765de82c971ce40c1421d6a9b125914e5
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-05-30 12:12:37 +0100

    use a get route for greeting

--- app.rb
+++ app.rb
@@ -1,31 +1,35 @@
 require 'sinatra/base'
 
 class BirthdayGreeter < Sinatra::Base
+  enable :sessions
+  
   get '/' do
     erb(:index)
   end
 
-  post '/greet' do
+  post '/birthday' do
     day = params[:day]
     month = params[:month]
     name = params[:name]
 
     if day.empty?
-      @greeting = "I have no idea whether it's your birthday because you didn't tell me the day!"
-      return erb(:greet)
+      $greeting = "I have no idea whether it's your birthday because you didn't tell me the day!"
+      redirect '/greet'
     end
 
     days_until_birthday = calculate_days_until_birthday(day, month)
     if days_until_birthday.zero?
-      @greeting = "It's your birthday #{name}!!!!!!!!!!!!!!!!!!!!!!!!!"
+      $greeting = "It's your birthday #{name}!!!!!!!!!!!!!!!!!!!!!!!!!"
     else
-      @greeting = "It is not your birthday #{name} :(. Don't be too sad though, it's in #{days_until_birthday} days"
+      $greeting = "It is not your birthday #{name} :(. Don't be too sad though, it's in #{days_until_birthday} days"
     end
 
-    erb(:greet)
+    redirect '/greet'
   end
 
-
+  get '/greet' do
+    erb(:greet)
+  end 
 
   def calculate_days_until_birthday(day, month)
     today = Date.today
