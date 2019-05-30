06bbc09cc5912c8c1775f0f78be71f5f592bb89c
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-05-30 12:04:45 +0100

    refactor to use modular sinatra & capybara built in DSL

--- app.rb
+++ app.rb
@@ -1,32 +1,39 @@
-require 'sinatra'
+require 'sinatra/base'
 
-get '/' do
-  erb(:index)
-end
-
-post '/greet' do
-  day = params[:day]
-  month = params[:month]
-  name = params[:name]
-
-  if day.empty?
-    @greeting = "I have no idea whether it's your birthday because you didn't tell me the day!"
-    return erb(:greet)
+class BirthdayGreeter < Sinatra::Base
+  get '/' do
+    erb(:index)
   end
 
-  days_until_birthday = calculate_days_until_birthday(day, month)
-  if days_until_birthday.zero?
-    @greeting = "It's your birthday #{name}!!!!!!!!!!!!!!!!!!!!!!!!!"
-  else
-    @greeting = "It is not your birthday #{name} :(. Don't be too sad though, it's in #{days_until_birthday} days"
+  post '/greet' do
+    day = params[:day]
+    month = params[:month]
+    name = params[:name]
+
+    if day.empty?
+      @greeting = "I have no idea whether it's your birthday because you didn't tell me the day!"
+      return erb(:greet)
+    end
+
+    days_until_birthday = calculate_days_until_birthday(day, month)
+    if days_until_birthday.zero?
+      @greeting = "It's your birthday #{name}!!!!!!!!!!!!!!!!!!!!!!!!!"
+    else
+      @greeting = "It is not your birthday #{name} :(. Don't be too sad though, it's in #{days_until_birthday} days"
+    end
+
+    erb(:greet)
   end
 
-  erb(:greet)
-end
 
-def calculate_days_until_birthday(day, month)
-  today = Date.today
-  birthday = Date.new(today.year, month.to_i, day.to_i)
-  return (birthday - today).to_i if birthday >= today
-  return (birthday.next_year - today).to_i
+
+  def calculate_days_until_birthday(day, month)
+    today = Date.today
+    birthday = Date.new(today.year, month.to_i, day.to_i)
+    return (birthday - today).to_i if birthday >= today
+    return (birthday.next_year - today).to_i
+  end
+
+  run! if app_file == $0
+
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,11 +1,4 @@
-require 'capybara/dsl'
-require 'selenium-webdriver'
-include Capybara::DSL
-Capybara.default_driver = :selenium
-
-
-describe 'app' do
-  
+feature 'BirthdayGreeter' do
   let(:days_until_birthday) do
     today = Date.today
     birthday = Date.new(today.year, 04, 13)
@@ -14,80 +7,78 @@ describe 'app' do
     else
       return (birthday.next_year - today).to_i
     end
-  end
-
-  context 'homepage' do
-    before (:context) do
-      visit('http://localhost:9393')
+  end 
+  feature 'homepage' do
+    background do
+      visit('/')
     end
-    it 'contains a title' do
+    
+    scenario 'contains a title' do
       expect(page).to have_css('.title')
     end
-    it 'contains a name input box' do
+    scenario 'contains a name input box' do
       expect(page).to have_css('input[name=name][type=text]')
     end
-    it 'contains a birthday day input box' do
+    scenario 'contains a birthday day input box' do
       expect(page).to have_css('input[name=day][type=text]')
     end
-    it 'contains a birthday month dropdown' do
+    scenario 'contains a birthday month dropdown' do
       expect(page).to have_css('select[name=month]')
     end
-    it 'contains a go button' do
+    scenario 'contains a go button' do
       expect(page).to have_css('input[name=go][type=submit]')
     end
+
   end
 
-  context '/greet' do
-
-    context 'when name input was empty' do
-      before(:context) do
-        visit('http://localhost:9393')
+  feature '/greet' do
+    feature 'when name input was empty' do
+      background do
+        visit('/')
         page.fill_in('day', with: '13')
         page.select('April', from: 'month')
         page.click_button('Go')
       end
 
-      it "greets without a name" do
+      scenario "greets without a name" do
         expect(page).to have_css('h1', text: "It is not your birthday :(. Don't be too sad though, it's in #{days_until_birthday} days")
       end
     end
 
-    context 'when day input was empty' do
-      before(:context) do
-        visit('http://localhost:9393')
+    feature 'when day input was empty' do
+      background do
+        visit('/')
         page.fill_in('name', with: 'Peter')
         page.select('April', from: 'month')
         page.click_button('Go')
       end
 
-      it "complains at the user" do
+      scenario "complains at the user" do
         expect(page).to have_css('h1', text: "I have no idea whether it's your birthday because you didn't tell me the day!")
       end
-
     end
 
-    context "when it is not the person's birthday" do
-      before(:context) do
-        visit('http://localhost:9393')
+    feature "when it is not the person's birthday" do
+      background do
+        visit('/')
         page.fill_in('name', with: 'Peter')
         page.fill_in('day', with: '13')
         page.select('April', from: 'month')
         page.click_button('Go')
       end
 
-      it 'contains a title' do
+      scenario 'contains a title' do
         expect(page).to have_css('h1')
       end
 
-      it "says that it is not the person's birthday" do
-
+      scenario "says that it is not the person's birthday" do
         expect(page).to have_css('h1', text: "It is not your birthday Peter :(. Don't be too sad though, it's in #{days_until_birthday} days")
       end
     end
 
-    context "when it is the person's birthday" do
-      before(:context) do
-        visit('http://localhost:9393')
+    feature "when it is the person's birthday" do
+      background do
+        visit('/')
         day = Time.now.strftime('%d')
         month = Time.now.strftime('%B')
         page.fill_in('name', with: 'Peter')
@@ -96,13 +87,118 @@ describe 'app' do
         page.click_button('Go')
       end
 
-      it 'contains a title' do
+      scenario 'contains a title' do
         expect(page).to have_css('h1')
       end
 
-      it "says that it is the person's birthday" do
+      scenario "says that it is the person's birthday" do
         expect(page).to have_css('h1', text: "It's your birthday Peter!!!!!!!!!!!!!!!!!!!!!!!!!")
       end
     end
   end
 end
+
+
+# describe 'app' do
+  
+#   visit '/'
+#   let(:days_until_birthday) do
+#     today = Date.today
+#     birthday = Date.new(today.year, 04, 13)
+#     if birthday >= today
+#       return (birthday - today).to_i
+#     else
+#       return (birthday.next_year - today).to_i
+#     end
+#   end
+
+#   context 'homepage' do
+#     before (:context) do
+#       visit('http://localhost:9393')
+#     end
+#     it 'contains a title' do
+#       expect(page).to have_css('.title')
+#     end
+#     it 'contains a name input box' do
+#       expect(page).to have_css('input[name=name][type=text]')
+#     end
+#     it 'contains a birthday day input box' do
+#       expect(page).to have_css('input[name=day][type=text]')
+#     end
+#     it 'contains a birthday month dropdown' do
+#       expect(page).to have_css('select[name=month]')
+#     end
+#     it 'contains a go button' do
+#       expect(page).to have_css('input[name=go][type=submit]')
+#     end
+#   end
+
+#   context '/greet' do
+
+#     context 'when name input was empty' do
+#       before(:context) do
+#         visit('http://localhost:9393')
+#         page.fill_in('day', with: '13')
+#         page.select('April', from: 'month')
+#         page.click_button('Go')
+#       end
+
+#       it "greets without a name" do
+#         expect(page).to have_css('h1', text: "It is not your birthday :(. Don't be too sad though, it's in #{days_until_birthday} days")
+#       end
+#     end
+
+#     context 'when day input was empty' do
+#       before(:context) do
+#         visit('http://localhost:9393')
+#         page.fill_in('name', with: 'Peter')
+#         page.select('April', from: 'month')
+#         page.click_button('Go')
+#       end
+
+#       it "complains at the user" do
+#         expect(page).to have_css('h1', text: "I have no idea whether it's your birthday because you didn't tell me the day!")
+#       end
+
+#     end
+
+#     context "when it is not the person's birthday" do
+#       before(:context) do
+#         visit('http://localhost:9393')
+#         page.fill_in('name', with: 'Peter')
+#         page.fill_in('day', with: '13')
+#         page.select('April', from: 'month')
+#         page.click_button('Go')
+#       end
+
+#       it 'contains a title' do
+#         expect(page).to have_css('h1')
+#       end
+
+#       it "says that it is not the person's birthday" do
+
+#         expect(page).to have_css('h1', text: "It is not your birthday Peter :(. Don't be too sad though, it's in #{days_until_birthday} days")
+#       end
+#     end
+
+#     context "when it is the person's birthday" do
+#       before(:context) do
+#         visit('http://localhost:9393')
+#         day = Time.now.strftime('%d')
+#         month = Time.now.strftime('%B')
+#         page.fill_in('name', with: 'Peter')
+#         page.fill_in('day', with: day)
+#         page.select(month, from: 'month')
+#         page.click_button('Go')
+#       end
+
+#       it 'contains a title' do
+#         expect(page).to have_css('h1')
+#       end
+
+#       it "says that it is the person's birthday" do
+#         expect(page).to have_css('h1', text: "It's your birthday Peter!!!!!!!!!!!!!!!!!!!!!!!!!")
+#       end
+#     end
+#   end
+# end
